4cedfb8eb55898136240e05e4785192f33f8d0ae
dExIT <dexit@dyc.lv>
2023-07-07 22:07:01 +0100

    simpel changes.. cant run

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,53 +12,54 @@ const resolvers = {
                 return user
             }
     },
-    Mutation:{
+    Mutation: {
         createUser: async (parent, { user: userInput }) => {
-            const user = await User.create(userInput);
-            const token = signToken(user);
-            return { 
-                token,
-                 user
-             };
+          const user = await User.create(userInput);
+          const token = signToken(user);
+          return { token, user };
         },
         loginUser: async (parent, { email, password }) => {
-            const user = await User.findOne({ 
-                email 
-            });
-
-            if (!user) { throw new AuthenticationError("I cant agree or deny the existence of this email");    }
-            const passwordOk = await user.isCorrectPassword(password);
-            if (!passwordOk) { throw new AuthenticationError("Incorrect credentials");  }
-        
-            const token = signToken(user);
-            return { token, user };
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError("No user found with this email address");
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError("Incorrect credentials");
+          }
+    
+          const token = signToken(user);
+    
+          return { token, user };
         },
         saveBook: async (parent, { book }, context) => {
-            if (!context.user) { throw new AuthenticationError("You must be logged in"); }
-            const userBookAdd = await User.findOneAndUpdate(
-                    { _id: context.user_id},
-                     { $addToSet: { 
-                        savedBooks: book 
-                        }
-                     },
-                     { new: true, runValidators: true }
-                ); 
-                return userBookAdd;           
+          if (!context.user) {
+            throw new AuthenticationError("You must be logged in");
+          }
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { savedBooks: book } },
+            { new: true, runValidators: true }
+          );
+          return updatedUser;
         },
+    
         deleteBook: async (parent, { bookId }, context) => {
-            if (!context.user) { throw new AuthenticationError("You must be logged in"); }
-            const userBookPull = await User.findOneAndUpdate(
-                { _id: context.user_id},
-                 { $pull: { 
-                    savedBooks: {
-                        bookId: bookId
-                        }
-                     }
-                },
-                 { new: true}
-            ); 
-            return userBookPull;           
+          if (!context.user) {
+            throw new AuthenticationError("You must be logged in");
+          }
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId: bookId } } },
+            { new: true }
+          );
+    
+          return updatedUser
         },
-
-    },
+    
+    
+      },
 };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+
+
 const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require("./utils/auth");
+//const routes = require('./routes');
 const path = require('path');
 const db = require('./config/connection');
-//const routes = require('./routes');
-const { typeDefs, resolvers } = require('./schemas');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer({     typeDefs,     resolvers,  context: authMiddleware, });
-const { authMiddleware } = require("./utils/auth");
+const server = new ApolloServer({     
+  typeDefs,
+  resolvers,
+ // context: authMiddleware
+});
+
 
 
 app.use(express.urlencoded({ extended: true }));
