55427ad010b0e958bde1d832ad06bf204dda1823
dExIT <dexit@dyc.lv>
2023-07-07 22:12:36 +0100

    added from coursework amterial

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,18 @@ import {  ApolloClient,  InMemoryCache,  ApolloProvider,  createHttpLink } from 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
-
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 function App() {
   return (
     <Router>
