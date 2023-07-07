102183641ad684fdca06f76b0b5e30845207ef53
dExIT <dexit@dyc.lv>
2023-07-07 22:51:04 +0100

    removed all API.js old code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,4 @@
+/*
 // route to get logged in user's info (needs the token)
 export const getMe = (token) => {
   return fetch('/api/users/me', {
@@ -52,6 +53,8 @@ export const deleteBook = (bookId, token) => {
 
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
+
+*/
 export const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
