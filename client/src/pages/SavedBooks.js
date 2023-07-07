0acfcec18ac2929de150a33350ec8c0d6f13e677
dExIT <dexit@dyc.lv>
2023-07-07 22:58:10 +0100

    change useeffect and add login error message

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -20,32 +20,21 @@ const SavedBooks = () => {
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
+  const {loading, data} = useQuery(GET_ME);
+  const [removeBook] = useMutation(REMOVE_BOOK)
+  const userData= data?.me || {}
+  const [books, setBooks] = useState();
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
+  useEffect(()=>{
+    setBooks(data?.me.savedBooks);
+  },[data]);
+  if (!userData?.username) {
+    return (
+      <h4>
+        You need to be logged. 
+      </h4>
+    );
+  }  
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
