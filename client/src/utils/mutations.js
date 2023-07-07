2a697faeb0f9ddc31362fa3ef41864c1d1eeb9cf
dExIT <dexit@dyc.lv>
2023-07-07 22:23:18 +0100

    trying the new way by descriping all correctly

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,13 +3,13 @@ import { gql } from '@apollo/client';
 
 export const ADD_USER = gql`
 
-  mutation (($username: String!, $email: String!, $password: String!) {
-    addUser(user: $user) {
+  mutation ($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
       token
       user {
         _id
         username
-        email
+        
       }
     }
   }
