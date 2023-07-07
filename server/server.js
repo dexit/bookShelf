6a352c05a07ed4cf654774ca840cb0dcb4376e73
dExIT <dexit@dyc.lv>
2023-07-07 20:32:36 +0100

    pre-written code for typeDefs and resolvers

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 //const routes = require('./routes');
-
+// not yet : const { typeDefs, resolvers } = require('./schemas')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
