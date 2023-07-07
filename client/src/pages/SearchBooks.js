4540307c8d5dd938802751105a3634d74f0efcd2
dExIT <dexit@dyc.lv>
2023-07-07 23:04:58 +0100

    Setup the whole save and submit form handling

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -66,6 +66,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    console.log(bookToSave);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -75,13 +76,12 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({variables: {bookData: bookToSave}});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
