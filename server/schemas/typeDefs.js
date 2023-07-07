e55e4059c23b74b02c3f12bad6c3ec0809cc4139
dExIT <dexit@dyc.lv>
2023-07-07 22:25:01 +0100

    auth needs to be ID

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,7 @@ const { gql }     = require('apollo-server-express');
 
 const typeDefs = gql`
                         type Auth {
-                                    token: String!
+                                    token: ID!
                                     user: User
                         },
                         type User {
